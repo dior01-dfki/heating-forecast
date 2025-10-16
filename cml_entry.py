34af22aedc249a0eef4db8d534183864b154317c
Dinar <dior00002@WINDORAZGALIYEV-97424.localdomain>
2025-10-16 21:09:46 +0200

    upds as per issue47

--- cml_entry.py
+++ cml_entry.py
@@ -1,4 +1,4 @@
-from forcateri.reporting.clearmlreporter.clearmlreporter import ClearMLReporter
+from forcateri.reporting.clearmlreporter import ClearMLReporter
 from src import project_root
 
 cml_reporter = ClearMLReporter(config_name='pipeline', project_root=project_root)
