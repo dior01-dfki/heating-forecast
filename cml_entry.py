a493af4398192c5d3f51cfe333e635be62cf897e
dior01-dfki <dior01@dfki.de>
2026-01-09 16:42:18 +0100

    the branch that will be used for training

--- cml_entry.py
+++
@@ -1,9 +0,0 @@
-from forcateri.reporting.clearmlreporter.clearmlreporter import ClearMLReporter
-from src import project_root
-
-cml_reporter = ClearMLReporter(config_name='pipeline', project_root=project_root)
-print(cml_reporter.config)
-print("\n\n\n")
-print(cml_reporter.args)
-cml_reporter.args.append(('config', 'pipeline'))
-cml_reporter.execute_task_enq()
