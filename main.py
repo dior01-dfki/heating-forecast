007165bd530a5c1344d009af0bf1fac60f60be7d
dior01-dfki <dior01@dfki.de>
2026-01-12 19:49:01 +0100

    .

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ from clover.decorator import connect_config
 import logging 
 
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 OFFSET, TIME_STEP = TimeSeries.ROW_INDEX_NAMES
